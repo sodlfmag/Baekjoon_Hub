b43e9ba8a859252dcc7f96bc651c4465db7f462f
sodlfmag <92130698+sodlfmag@users.noreply.github.com>
2023-05-08 21:55:36 +0900

    [level 1] Title: 약수의 합, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = 0
+    arr = set()
+    
+    if( n == 0):
+        return 0
+    
+    if(n == 1):
+        return 1
+    
+    for i in range(1, n//2 +1):
+        if(n % i == 0):
+            arr.add(i)
+            arr.add(n // i)   
+            
+    answer = sum(arr)
+    return answer
